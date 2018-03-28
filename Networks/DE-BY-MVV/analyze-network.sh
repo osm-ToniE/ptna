f0013f494dccd771a9d6da166f23710133d4eefe
Toni Erdmann <Toni.Erdmann@web.de>
2018-03-28 19:48:25 +0200

    analysis script to use wiki-page.pl
    the script now uses the wiki-page.pl script to automatically upload the analysis result to OSM wiki.
    upload is done only if the analysis succeeded (file size > 0) and the diff between old and new version is > a specific limit

--- Networks/DE-BY-MVV/analyze-network.sh
+++ Networks/DE-BY-MVV/analyze-network.sh
@@ -20,38 +20,68 @@ XML_FILE="$PREFIX-Data.xml"
 WIKI_FILE="$PREFIX-Analysis.wiki"
 HTML_FILE="$PREFIX-Analysis.html"
 
+WIKI_ANALYSIS_PAGE="München/Transportation/Analyse"
+WIKI_ROUTES_PAGE="München/Transportation/MVV-Linien-gesamt"
+WIKI_FILE_DIFF="196"
+
 #
 # 
 #
 
-if [ -f $WIKI_FILE ]
-then
-    mv $WIKI_FILE $WIKI_FILE.save
-    rm -f $WIKI_FILE.diff
-fi
+#if [ -f $WIKI_FILE ]
+#then
+#    mv $WIKI_FILE $WIKI_FILE.save
+#    rm -f $WIKI_FILE.diff
+#fi
 
 if [ "$call_wget" = "yes" ]
 then
+    echo "Calling WGET for $PREFIX"
     wget "$OVERPASS_QUERY" -O $XML_FILE
 fi
 
-analyze-routes.pl -v --wiki --check-access --check-name --check-stop-position --check-sequence --check-version --check-wide-characters --coloured-sketchline --expect-network-long --network-long-regex="$NETWORK_LONG" --network-short-regex="$NETWORK_SHORT" --lines-csv=$CSV_FILE --routes=$XML_FILE > $WIKI_FILE
-
 # --check-bus-stop 
 
-if [ -f $WIKI_FILE.save ]
+analyze-routes.pl -v --wiki --check-access --check-name --check-stop-position --check-sequence --check-version --check-wide-characters --coloured-sketchline --expect-network-long --network-long-regex="$NETWORK_LONG" --network-short-regex="$NETWORK_SHORT" --lines-csv=$CSV_FILE --routes=$XML_FILE > $WIKI_FILE
+
+if [ -s $WIKI_FILE ]
 then
-    diff $WIKI_FILE $WIKI_FILE.save                                 | \
-    sed -e 's/ <small>([^)]*)<\/small>//g'                          | \
-    sed -e 's/{{Relation|//g'                                       | \
-    sed -e 's/\[\[Image:Osm_element_relation.svg|20px\]\]//g'       | \
-    sed -e 's/\[\[Image:Osm_element_way.svg|20px\]\]//g'            | \
-    sed -e 's/\[\[Image:Osm_element_node.svg|20px\]\]//g'           | \
-    sed -e 's/\[http:..osm.org.relation.[0-9]* //g'                 | \
-    sed -e 's/\[http:..osm.org.way.[0-9]* //g'                      | \
-    sed -e 's/\[http:..osm.org.node.[0-9]* //g'                     | \
-    sed -e 's/\], //g'                                              | \
-    sed -e 's/\]//g'                                                | \
-    sed -e 's/}}//g'                                                > $WIKI_FILE.diff
+    if [ "$call_wget" = "yes" ]
+    then
+        echo "Reading old Wiki analysis page $WIKI_ANALYSIS_PAGE"
+        wiki-page.pl --pull --page=$WIKI_ANALYSIS_PAGE --file=$WIKI_FILE.old
+        
+        diff $WIKI_FILE $WIKI_FILE.old > $WIKI_FILE.diff
+        
+        ls -l $WIKI_FILE.diff
+        
+        diffsize=$(ls -l $WIKI_FILE.diff 2> /dev/null | awk '{print $5}')
+    
+        if [ "$diffsize" -gt "$WIKI_FILE_DIFF" ]
+        then
+            echo "Writing new Wiki analysis page $WIKI_ANALYSIS_PAGE"
+            wiki-page.pl --push --page=$WIKI_ANALYSIS_PAGE --file=$WIKI_FILE --summary="automatic update by analyze-routes"
+        fi
+    fi
+else
+    echo $WIKI_FILE is empty
+    ls -l $WIKI_FILE
 fi
+
+
+#if [ -f $WIKI_FILE.save ]
+#then
+#    diff $WIKI_FILE $WIKI_FILE.save                                 | \
+#    sed -e 's/ <small>([^)]*)<\/small>//g'                          | \
+#    sed -e 's/{{Relation|//g'                                       | \
+#    sed -e 's/\[\[Image:Osm_element_relation.svg|20px\]\]//g'       | \
+#    sed -e 's/\[\[Image:Osm_element_way.svg|20px\]\]//g'            | \
+#    sed -e 's/\[\[Image:Osm_element_node.svg|20px\]\]//g'           | \
+#    sed -e 's/\[http:..osm.org.relation.[0-9]* //g'                 | \
+#    sed -e 's/\[http:..osm.org.way.[0-9]* //g'                      | \
+#    sed -e 's/\[http:..osm.org.node.[0-9]* //g'                     | \
+#    sed -e 's/\], //g'                                              | \
+#    sed -e 's/\]//g'                                                | \
+#    sed -e 's/}}//g'                                                > $WIKI_FILE.diff
+#fi
 
