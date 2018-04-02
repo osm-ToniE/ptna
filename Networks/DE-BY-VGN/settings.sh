c9b8a249e596a0c9ffbb904bfd5208378285a141
Toni Erdmann <Toni.Erdmann@web.de>
2018-04-02 12:33:47 +0200

    change search criteria for VGB
    change criteria from list of Kreise/Landkreise to boundary=public_transport and name=<name of network>

--- Networks/DE-BY-VGN/settings.sh
+++ Networks/DE-BY-VGN/settings.sh
@@ -6,7 +6,7 @@
 
 PREFIX="DE-BY-VGN"
 
-OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name~'Amberg|Ansbach|Bamberg|Bayreuth|Erlangen|Fürth|Nürnberg|Schwabach|Donau-Ries|Forchheim|Haßberge|Kitzingen|Lichtenfels|Neumarkt in der Oberpfalz|Neustadt an der Aisch-Bad Windsheim|Roth|Weißenburg-Gunzenhausen|Eichstätt|Kelheim|Neustadt an der Waldnaab|Regensburg']->.L; rel(area.L)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)']->.R; rel(br.R); out; rel.R; out; rel(r.R); out; way(r.R); out; node(r.R); out;"
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=public_transport][name='Verkehrsverbund Großraum Nürnberg GmbH']->.L; rel(area.L)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)']->.R; rel(br.R); out; rel.R; out; rel(r.R); out; way(r.R); out; node(r.R); out;"
 NETWORK_LONG="Verkehrsverbund Großraum Nürnberg"
 NETWORK_SHORT="VGN"
 
