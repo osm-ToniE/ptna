605a14a79baadc5e5a0b356081309bf6c449d16a
Toni Erdmann <Toni.Erdmann@web.de>
2018-04-09 22:36:28 +0200

    enhance Landshut bus route file

--- Networks/DE-BY-LA/settings.sh
+++ Networks/DE-BY-LA/settings.sh
@@ -6,8 +6,8 @@
 
 PREFIX="DE-BY-LA"
 
-OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name~'Landshut']->.L; rel(area.L)[route~'(bus|tram|train|subway|light_rail|trolleybus|ferry|monorail|aerialway|share_taxi|funicular)']->.R; rel(br.R); out; rel.R; out; rel(r.R); out; way(r.R); out; node(r.R); out;"
-NETWORK_LONG="Landshuter Regionalbusnetz"
+OVERPASS_QUERY="http://overpass-api.de/api/interpreter?data=area[boundary=administrative][admin_level=6][name~'Landshut']->.L; rel(area.L)[route~'bus']->.R; rel(br.R); out; rel.R; out; rel(r.R); out; way(r.R); out; node(r.R); out;"
+NETWORK_LONG="Stadtwerke Landshut|Landshuter Regionalbusnetz"
 NETWORK_SHORT="LA"
 
 WIKI_ANALYSIS_PAGE="Landshut/Transportation/Analyse"
