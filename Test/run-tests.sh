a601da38a7399b0e0a29e972f0a2f93c323d6655
Toni Erdmann <Toni.Erdmann@web.de>
2018-05-01 17:27:58 +0200

    reorganize Test environment
    there is a global script "run-tests.sh" which runs all test script called "*.script"
    ignore not only *.html.save or *.html.diff but all *.save and all *.diff files
    include file for expected result "*.expect"

--- Test/run-tests.sh
+++ Test/run-tests.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Issue #17
+ERRORS=0
 
-analyze-routes.pl --max-error=10 --check-access --check-name --check-stop-position --check-sequence --check-version --check-wide-characters \
-                  --multiple-ref-type-entries=allow --positive-notes --coloured-sketchline --verbose --debug \
-                  --osm-xml-file RBO-6209-006-issue-17.osm > RBO-6209-006-issue-17.html 2> RBO-6209-006-issue-17.log
+for script in *.script
+do
+    bash ./$script
+    
+    ERRORS=$(( $ERRORS + $? ))
+done
 
-# 
+echo ""
+echo "Tests ended with $ERRORS errors"
+exit $ERRORS
 
