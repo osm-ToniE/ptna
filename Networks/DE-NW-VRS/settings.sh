8995eb973ee1e8b1373305b1c582610ee1180d65
Toni Erdmann <Toni.Erdmann@web.de>
2018-06-10 18:20:10 +0200

    adapt some settings regarding 'network' analysis

--- Networks/DE-NW-VRS/settings.sh
+++ Networks/DE-NW-VRS/settings.sh
@@ -16,7 +16,7 @@ ANALYSIS_PAGE="VRS/Analyse"
 WIKI_ROUTES_PAGE="VRS/Analyse/VRS-Linien"
 FILE_DIFF="200"
 
-ANALYSIS_OPTIONS="--max-error=10 --check-access --check-name --check-stop-position --check-sequence --check-osm-separator --multiple-ref-type-entries=analyze --relaxed-begin-end-for=train,light_rail,tram --coloured-sketchline --expect-network-long"
+ANALYSIS_OPTIONS="--max-error=10 --check-access --check-name --check-stop-position --check-sequence --check-osm-separator --multiple-ref-type-entries=analyze --relaxed-begin-end-for=train,light_rail,tram --coloured-sketchline --expect-network-short"
 
 # --positive-notes
 # --check-bus-stop 
