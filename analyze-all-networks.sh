b559ab863506bb828c7734f0e93164d994481387
Toni Erdmann <Toni.Erdmann@web.de>
2018-07-29 19:24:09 +0200

    allow some networks to ba analyzed only on Sunday
    A new Network "EU-Flixbus" requires a lot of processing.
    This shallbe done only once a week.

--- analyze-all-networks.sh
+++ analyze-all-networks.sh
@@ -8,17 +8,33 @@ WD=$PWD
 
 PATH=$PWD/bin:$PATH
 
+# day of week: 1 ... 7 (7 = Sunday)
+DOW=$(date +%u)
+
 for A in  Networks/*
 do
     echo
     echo $(date "+%Y-%m-%d %H:%M:%S") "$A"
     echo
     
-    cd $A
-
-    analyze-network.sh $*
-
-    cd $WD
-
+    ANALYZE="yes"
+    
+    NETWORK=$(basename $A)
+    AREA=${NETWORK%%-*}
+    
+    if [ "$AREA" = "EU" -a $DOW -ne 7 ]
+    then
+        ANALYZE="no"
+    fi
+    
+    if [ "$ANALYZE" = "yes" ]
+    then
+        cd $A
+    
+        analyze-network.sh $*
+    
+        cd $WD
+    fi
+    
 done
 
