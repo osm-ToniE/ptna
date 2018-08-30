81ab31c70dec68476596d69e7f03abb717f4ea81
Toni Erdmann <Toni.Erdmann@web.de>
2018-08-30 19:02:08 +0200

    adapt check of diff file size to actual site of 104/204 bytes
    upload *.html file only if the diff between old and new analysis results is bigger than that

--- Networks/DE-BY-RVV/settings.sh
+++ Networks/DE-BY-RVV/settings.sh
@@ -12,7 +12,7 @@ NETWORK_SHORT="RVV"
 
 ANALYSIS_PAGE="Regensburg/Transportation/Analyse"
 WIKI_ROUTES_PAGE="Regensburg/Transportation/Analyse/DE-BY-RVV-Linien"
-FILE_DIFF="200"
+FILE_DIFF="204"
 
 ANALYSIS_OPTIONS="--max-error=10 --check-access --check-name --check-stop-position --check-sequence --check-version --check-osm-separator --check-motorway-link --multiple-ref-type-entries=allow --positive-notes --coloured-sketchline"
 
