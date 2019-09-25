480945d1ae53fb26a4741a8880291b71ee8fb5b5
ToniE <osm-ToniE@web.de>
2019-09-25 11:25:29 +0200

    print also timezone when printing date and time
    forgot one appearance, the usual one, in the code

--- bin/ptna-network.sh
+++ bin/ptna-network.sh
@@ -378,7 +378,7 @@ then
                             echo $(date "+%Y-%m-%d %H:%M:%S") "Updating results HTML file '$RESULTS_HTML'"
 
                             NEW_OSM_Base_Time="$(awk '/OSM-Base Time : .* UTC/ { print $4 "T" $5 "Z"; }' $WORK_LOC/$HTML_FILE)"
-                            NEW_Local_OSM_Base_Time="$(date --date="$NEW_OSM_Base_Time" '+%d.%m.%Y %H:%M:%S')"
+                            NEW_Local_OSM_Base_Time="$(date --date="$NEW_OSM_Base_Time" '+%d.%m.%Y %H:%M:%S %Z')"
     
                             sed -i -e "s/^\(.*$PREFIX-datadate.*\)<time .*\(<.time>.*\)$/\1<time datetime='$NEW_OSM_Base_Time'>$NEW_Local_OSM_Base_Time\2/" \
                                    -e "s/^\(.*$PREFIX-analyzed.*\)<time .*\(<.time>.*\)$/\1<time datetime='$NEW_OSM_Base_Time'>$NEW_Local_OSM_Base_Time\2/" \
