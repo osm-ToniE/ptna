9ff834c366af8ff0658be650b86b39ee745fd6ad
Toni Erdmann <Toni.Erdmann@web.de>
2022-10-28 19:23:49 +0200

    set verbose mode of wget to --no-verbose

--- bin/ptna-network.sh
+++ bin/ptna-network.sh
@@ -112,7 +112,7 @@ else
     OSM_XML_FILE_ABSOLUTE="$WORK_LOC/$PREFIX-Data.xml"
 fi
 
-CALL_PARAMS="--server-response --tries=2 --wait=10 --random-wait"
+CALL_PARAMS="--server-response --tries=2 --wait=10 --random-wait --no-verbose"
 
 #
 #
