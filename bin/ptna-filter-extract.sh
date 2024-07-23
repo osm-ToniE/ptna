bc907560ea300d7364aca7e45fe8cb952ed6675f
Toni Erdmann <Toni.Erdmann@web.de>
2024-07-23 14:18:23 +0200

    improve filter

--- bin/ptna-filter-extract.sh
+++ bin/ptna-filter-extract.sh
@@ -30,8 +30,8 @@ then
         osmium tags-filter -v -F pbf -f osm -O -o "$TARGET" \
                --output-header="generator=https://ptna.openstreetmap.de osmosis_replication_timestamp=$TS" "$TARGET-Data-filtered.osm.pbf" \
                -i r/route=tracks,railway,bicycle,mtb,hiking,road,foot,inline_skates,canoe,detour,fitness_trail,horse,waterway,motorboat,boat,nordic_walking,pipeline,piste,power,running,ski,snowmobile,cycling,historic,motorcycle,riding,junction \
-                  r/type=defaults,area,person,treaty,cemetery,election,level,restriction,boundary,building,building:part,organization,set,bridge,site,health,junction,right_of_way,dual_carriageway,street,associated_street,cluster,tunnel,tmc,TMC,tmc:point,traffic_signals,place_numbers,shop,group,collection r/type=*golf r/highway=pedestrian,service \
-                  historic power amenity boundary admin_level place tourism parking landuse building building:part room natural shop office craft man_made leisure playground
+                  r/type=defaults,area,person,treaty,cemetery,election,level,restriction,boundary,building,waterway,building:part,organization,set,bridge,site,health,junction,right_of_way,dual_carriageway,street,associated_street,cluster,tunnel,tmc,TMC,tmc:point,traffic_signals,place_numbers,shop,group,collection r/type=*golf r/highway=pedestrian,service \
+                  cemetery historic power amenity boundary admin_level place tourism parking landuse building room natural shop office craft man_made leisure playground
 
         osmium_ret=$?
 
