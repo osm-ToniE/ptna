3a05eb6e41ea772844eaae7803dc69247a8f969f
Toni Erdmann <Toni.Erdmann@web.de>
2024-07-23 10:52:57 +0200

    delete option --clean from command (does not know that option)

--- bin/ptna-split-extract.sh
+++ bin/ptna-split-extract.sh
@@ -18,8 +18,7 @@ if [ -n "$SOURCE" -a -f "$SOURCE" -a -s "$SOURCE" -a -n "$CONFIG" -a -f "$CONFIG
 then
     echo $(date "+%Y-%m-%d %H:%M:%S %Z") "Call 'osmium extract' for '$SOURCE' and '$CONFIG'"
 
-    osmium extract --verbose --clean=user --clean=uid --clean=timestamp --clean=version --clean=changeset \
-                   --strategy=smart --option=types=any --option=complete-partial-relations=1 \
+    osmium extract --verbose --strategy=smart --option=types=any --option=complete-partial-relations=1 \
                    --config="$CONFIG" --overwrite --fsync "$SOURCE"
 
     osmium_ret=$?
