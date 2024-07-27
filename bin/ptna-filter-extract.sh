c7db0f900059fbbb1d2c9ec7fd12c9c0e9e77cb8
Toni Erdmann <Toni.Erdmann@web.de>
2024-07-27 12:27:03 +0200

    change name of temporary file; distinguish between 'xml' and 'pbf' output format

--- bin/ptna-filter-extract.sh
+++ bin/ptna-filter-extract.sh
@@ -11,11 +11,11 @@ then
 
     echo $(date "+%Y-%m-%d %H:%M:%S %Z") "'osmium replication timestamp' = '$TS'"
 
-    rm -f "$TARGET-Data-filtered.osm.pbf"
+    rm -f "$TARGET.filtered.$$"
 
-    echo $(date "+%Y-%m-%d %H:%M:%S %Z") "Call 'osmium tags-filter' for '$SOURCE' to filter with positive list"
+    echo $(date "+%Y-%m-%d %H:%M:%S %Z") "Call 'osmium tags-filter' for '$SOURCE' to filter with standard positive filter list"
 
-    osmium tags-filter -v -F pbf -f pbf -O -o "$TARGET-Data-filtered.osm.pbf" "$SOURCE" \
+    osmium tags-filter -v -F pbf -f pbf -O -o "$TARGET.filtered.$$" "$SOURCE" \
            r/type=*route r/type=public_transport,network r/abandoned:type r/disused:type r/suspended:type r/razed:type r/removed:type r/route_master r/route r/network r/name r/ref r/from r/to r/via r/public_transport:version r/ref_trips \
            public_transport highway=bus_stop,platform railway=stop,tram_stop,halt,station,platform route_ref gtfs:feed gtfs:route_id gtfs:stop_id gtfs:trip_id gtfs:trip_id:sample gtfs:shape_id
 
@@ -23,12 +23,18 @@ then
 
     echo $(date "+%Y-%m-%d %H:%M:%S %Z") "osmium returned $osmium_ret"
 
-    if [ -f "$TARGET-Data-filtered.osm.pbf" -a -s "$TARGET-Data-filtered.osm.pbf" ]
+    if [ -f "$TARGET.filtered.$$" -a -s "$TARGET.filtered.$$" ]
     then
-        echo $(date "+%Y-%m-%d %H:%M:%S %Z") "Call 'osmium tags-filter' for '$SOURCE' to filter with negative list"
+        echo $(date "+%Y-%m-%d %H:%M:%S %Z") "Call 'osmium tags-filter' for '$SOURCE' to filter with standard negative filter list"
 
-        osmium tags-filter -v -F pbf -f osm -O -o "$TARGET" \
-               --output-header="generator=https://ptna.openstreetmap.de osmosis_replication_timestamp=$TS" "$TARGET-Data-filtered.osm.pbf" \
+        OUTPUTFORMAT="${TARGET##*.}"
+        if [ "$OUTPUTFORMAT" == 'xml' ]
+        then
+            OUTPUTHEADER='--output-header=generator=https://ptna.openstreetmap.de osmosis_replication_timestamp=$TS"'
+        fi
+
+        osmium tags-filter -v -F pbf -f "$OUTPUTFORMAT" -O -o "$TARGET" \
+               "$OUTPUTHEADER" "$TARGET.filtered.$$" \
                -i r/route_master=tracks,railway,bicycle,mtb,hiking,road,foot,inline_skates,canoe,detour,fitness_trail,horse,waterway,motorboat,boat,nordic_walking,pipeline,piste,power,running,ski,snowmobile,cycling,historic,motorcycle,riding,junction \
                   r/route=tracks,railway,bicycle,mtb,hiking,road,foot,inline_skates,canoe,detour,fitness_trail,horse,waterway,motorboat,boat,nordic_walking,pipeline,piste,power,running,ski,snowmobile,cycling,historic,motorcycle,riding,junction,canyoning,climbing,sled,TMC \
                   r/type=defaults,area,destination_sign,enforcement,person,treaty,cemetery,pipeline,election,level,restriction,boundary,building,waterway,building:part,organization,set,bridge,site,health,junction,right_of_way,dual_carriageway,street,associated_street,cluster,tunnel,tmc,TMC,tmc:point,tmc:area,traffic_signals,place_numbers,shop,group,collection \
@@ -39,9 +45,9 @@ then
 
         echo $(date "+%Y-%m-%d %H:%M:%S %Z") "osmium returned $osmium_ret"
 
-        rm -f "$TARGET-Data-filtered.osm.pbf"
+        rm -f "$TARGET.filtered.$$"
     else
-        echo $(date "+%Y-%m-%d %H:%M:%S %Z") "Filtered file (prositive list) has not been created or is empty"
+        echo $(date "+%Y-%m-%d %H:%M:%S %Z") "Filtered file (positive list) has not been created or is empty"
         exit 1
     fi
 else
