6dd8fef12754628cf3722d65e376b40a1fbf1897
Toni Erdmann <Toni.Erdmann@web.de>
2024-07-27 14:48:40 +0200

    set propper name suffic '*.osm.pbf', not *.$$'

--- bin/ptna-filter-extract.sh
+++ bin/ptna-filter-extract.sh
@@ -5,11 +5,11 @@ TARGET=$2
 
 if [ -n "$SOURCE" -a -f "$SOURCE" -a -s "$SOURCE" -a -n "$TARGET" ]
 then
-    rm -f "$TARGET-[12].$$"
+    rm -f "$TARGET-[12].$$.osm.pbf"
 
     echo $(date "+%Y-%m-%d %H:%M:%S %Z") "Call 'osmium tags-filter' for '$SOURCE' to filter with standard positive filter list"
 
-    osmium tags-filter -v -F pbf -f pbf -O -o "$TARGET-1.$$" "$SOURCE" \
+    osmium tags-filter -v -F pbf -f pbf -O -o "$TARGET-1.$$.osm.pbf" "$SOURCE" \
            r/type=*route r/type=public_transport,network r/abandoned:type r/disused:type r/suspended:type r/razed:type r/removed:type r/route_master r/route r/network r/name r/ref r/from r/to r/via r/public_transport:version r/ref_trips \
            public_transport highway=bus_stop,platform railway=stop,tram_stop,halt,station,platform route_ref gtfs:feed gtfs:route_id gtfs:stop_id gtfs:trip_id gtfs:trip_id:sample gtfs:shape_id
 
@@ -17,7 +17,7 @@ then
 
     echo $(date "+%Y-%m-%d %H:%M:%S %Z") "osmium returned $osmium_ret"
 
-    if [ $osmium_ret -eq 0 -a -f "$TARGET-1.$$" -a -s "$TARGET-1.$$" ]
+    if [ $osmium_ret -eq 0 -a -f "$TARGET-1.$$.osm.pbf" -a -s "$TARGET-1.$$.osm.pbf" ]
     then
         echo $(date "+%Y-%m-%d %H:%M:%S %Z") "Call 'osmium tags-filter' for '$SOURCE' to filter with standard negative filter list"
 
@@ -33,8 +33,8 @@ then
             OUTPUTHEADER='--output-header=generator=https://ptna.openstreetmap.de osmosis_replication_timestamp=$TS"'
         fi
 
-        osmium tags-filter -v -F pbf -f "$OUTPUTFORMAT" -O -o "$TARGET-2.$$" \
-               "$OUTPUTHEADER" "$TARGET-1.$$" \
+        osmium tags-filter -v -F pbf -f "$OUTPUTFORMAT" -O -o "$TARGET-2.$$.osm.pbf" \
+               "$OUTPUTHEADER" "$TARGET-1.$$.osm.pbf" \
                -i r/route_master=tracks,railway,bicycle,mtb,hiking,road,foot,inline_skates,canoe,detour,fitness_trail,horse,waterway,motorboat,boat,nordic_walking,pipeline,piste,power,running,ski,snowmobile,cycling,historic,motorcycle,riding,junction \
                   r/route=tracks,railway,bicycle,mtb,hiking,road,foot,inline_skates,canoe,detour,fitness_trail,horse,waterway,motorboat,boat,nordic_walking,pipeline,piste,power,running,ski,snowmobile,cycling,historic,motorcycle,riding,junction,canyoning,climbing,sled,TMC \
                   r/type=defaults,area,destination_sign,enforcement,person,treaty,cemetery,pipeline,election,level,restriction,boundary,building,waterway,building:part,organization,set,bridge,site,health,junction,right_of_way,dual_carriageway,street,associated_street,cluster,tunnel,tmc,TMC,tmc:point,tmc:area,traffic_signals,place_numbers,shop,group,collection \
@@ -48,14 +48,14 @@ then
         if [ $osmium_ret -eq 0 ]
         then
             fsizep=$(stat -c '%s' "$SOURCE")
-            fsize1=$(stat -c '%s' "$TARGET-1.$$")
-            fsize2=$(stat -c '%s' "$TARGET-2.$$")
+            fsize1=$(stat -c '%s' "$TARGET-1.$$.osm.pbf")
+            fsize2=$(stat -c '%s' "$TARGET-2.$$.osm.pbf")
 
             echo $(date "+%Y-%m-%d %H:%M:%S %Z") "File sizes: source = '$fsizep', after positive filter = '$fsize1', target = '$fsize2'"
 
-            mv "$TARGET-2.$$" "$TARGET"
+            mv "$TARGET-2.$$.osm.pbf" "$TARGET"
 
-            rm -f "$TARGET-[12].$$"
+            rm -f "$TARGET-[12].$$.osm.pbf"
 
             if [ "$OUTPUTFORMAT" != 'xml' ]
             then
