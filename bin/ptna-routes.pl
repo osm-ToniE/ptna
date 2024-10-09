c44d6600be32edcc1ed70f74fea5bf7701a4d693
Toni Erdmann <Toni.Erdmann@web.de>
2024-10-09 13:50:23 +0200

    fix issue #143
    - treat 'busway*' = '*' as 'psv' = 'designated'

--- bin/ptna-routes.pl
+++ bin/ptna-routes.pl
@@ -4875,6 +4875,12 @@ sub noAccessOnWay {
             #
             printf STDERR "noAccessOnWay() : unclear access for all psv for way %d\n", $way_id       if ( $debug );
             return sprintf( "'psv:conditional'='%s'", $way_tag_ref->{'psv:conditional'} );
+        } elsif (  $way_tag_ref->{'busway'} || $way_tag_ref->{'busway:both'} || $way_tag_ref->{'busway:right'} || $way_tag_ref->{'busway:left'} ) {
+            #
+            # fine as this implies 'psv' = 'designated' (or 'bus' = 'designated')
+            #
+            printf STDERR "noAccessOnWay() : access for all psv for busway %d\n", $way_id       if ( $debug );
+            return '';
         } elsif ( $access_type && $way_tag_ref->{$access_type} ) {
             if ( $way_tag_ref->{$access_type} eq 'yes'         ||
                  $way_tag_ref->{$access_type} eq 'destination' ||
