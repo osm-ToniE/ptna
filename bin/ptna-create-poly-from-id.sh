b75fc947626a9b0135a0ba3aeceb2fda99accae8
Toni Erdmann <Toni.Erdmann@web.de>
2025-12-13 13:08:34 +0100

    improve creating *.poly to better match the workflow
    - large is mostly used when 'id' is given, so do not include '-large' in name
    - small is mostly used (is ever) if the data is shared, so include '-shared' in name

--- bin/ptna-create-poly-from-id.sh
+++ bin/ptna-create-poly-from-id.sh
@@ -5,6 +5,6 @@ ID="$2"
 
 if [ -n "$NAME" -a -n "$ID" ]
 then
-    curl "https://polygons.openstreetmap.fr/get_poly.py?id=$ID&params=0.20000-0.00050-0.00050" > $HOME/tmp/$NAME-large.poly
-    curl "https://polygons.openstreetmap.fr/get_poly.py?id=$ID&params=0.02000-0.00500-0.00500" > $HOME/tmp/$NAME-small.poly
+    curl "https://polygons.openstreetmap.fr/get_poly.py?id=$ID&params=0.20000-0.00050-0.00050" > $HOME/tmp/$NAME.poly
+    curl "https://polygons.openstreetmap.fr/get_poly.py?id=$ID&params=0.02000-0.00500-0.00500" > $HOME/tmp/$NAME-shared.poly
 fi
